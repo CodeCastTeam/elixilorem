fe604a8c82084a3aa1235ace9139d3ab397c7cda
Mike Martinson <mike.j.martinson@gmail.com>
2020-01-16 12:15:09 -0800

    Support >= 1.7, fix warnings and deprecations

--- lib/get_sum.ex
+++ lib/get_sum.ex
@@ -1,23 +1,27 @@
 defmodule Elixilorem.GetSum do
-
   @def_extension Application.get_env(:elixilorem, :extension, ".txt")
   @def_flavor Application.get_env(:elixilorem, :flavor, "lorem_ipsum")
-  @def_format Application.get_env(:elixilorem, :format, "text")
-  @def_joins Application.get_env(:elixilorem, :joins, [paragraphs: "\n", sentences: ". ", words: " "])
+  @def_joins Application.get_env(:elixilorem, :joins,
+               paragraphs: "\n",
+               sentences: ". ",
+               words: " "
+             )
 
   def def_ipsum_path do
-    to_string(:code.priv_dir(:elixilorem)) <> Application.get_env(:elixilorem, :ipsum_path, "/ipsums/")
+    to_string(:code.priv_dir(:elixilorem)) <>
+      Application.get_env(:elixilorem, :ipsum_path, "/ipsums/")
   end
 
   def get_block_sequence(type, count, %{flavor: flavor, format: nil}) do
     case get_sum_file(flavor) do
-      {:error, error} -> error
-      str ->
-        list = str |> strip(type) |> String.split(@def_joins[type], trim: true)
+      {:error, error} ->
+        error
+
+      _str ->
         list = get_sum_file(flavor) |> strip(type) |> String.split(@def_joins[type], trim: true)
         length = length(list)
 
-        gen_sequence({list, count, type}, :random.uniform(length), length, [])
+        gen_sequence({list, count, type}, :rand.uniform(length), length, [])
     end
   end
 
@@ -26,15 +30,17 @@ defmodule Elixilorem.GetSum do
   #
 
   def get_sum_file(nil), do: get_sum_file(@def_flavor)
+
   def get_sum_file(name) do
     try do
-      build_sum_filepath(name) |> File.read! |> String.strip
-    rescue error ->
-      {:error, error}
+      build_sum_filepath(name) |> File.read!() |> String.trim()
+    rescue
+      error ->
+        {:error, error}
     end
   end
 
-  def build_sum_filepath(name), do: def_ipsum_path <> name <> @def_extension
+  def build_sum_filepath(name), do: def_ipsum_path() <> name <> @def_extension
 
   #
   # "Why all this noise? Just return random paragraphs," my hypercritical inner voice says.
@@ -43,34 +49,45 @@ defmodule Elixilorem.GetSum do
   #
 
   defp gen_sequence({list, count, type}, idx, max, out) do
-    if idx > max, do: idx = 1
+    i = if idx > max, do: 1, else: idx
 
     if length(out) < count do
-      gen_sequence({list, count, type}, idx + 1, max, out ++ [:lists.nth(idx, list) |> String.strip])
+      gen_sequence(
+        {list, count, type},
+        i + 1,
+        max,
+        out ++ [:lists.nth(i, list) |> String.trim()]
+      )
     else
       gen_sequence({:ok, out, type})
     end
   end
 
-  defp gen_sequence({:ok, list, :paragraphs}), do: Enum.join(list, @def_joins[:paragraphs]) |> String.strip
+  defp gen_sequence({:ok, list, :paragraphs}),
+    do: Enum.join(list, @def_joins[:paragraphs]) |> String.trim()
+
   defp gen_sequence({:ok, list, :sentences}) do
     str = Enum.join(list, @def_joins[:sentences])
-    str <> @def_joins[:sentences] |> String.strip
+    (str <> @def_joins[:sentences]) |> String.trim()
   end
-  defp gen_sequence({:ok, list, :words}), do: Enum.join(list, @def_joins[:words]) |> String.strip |> String.capitalize
+
+  defp gen_sequence({:ok, list, :words}),
+    do: Enum.join(list, @def_joins[:words]) |> String.trim() |> String.capitalize()
 
   defp strip(str, :paragraphs), do: str
   defp strip(str, :sentences), do: str |> strip_paragraphs |> make_sentence
   defp strip(str, :words), do: str |> strip_paragraphs |> strip_sentences |> make_words
 
-  defp strip_paragraphs(str), do: str |> String.replace(@def_joins[:paragraphs], " ") |> String.replace("  ", " ")
+  defp strip_paragraphs(str),
+    do: str |> String.replace(@def_joins[:paragraphs], " ") |> String.replace("  ", " ")
+
   defp strip_sentences(str) do
-    sentence_join = @def_joins[:sentences] |> String.strip
+    sentence_join = @def_joins[:sentences] |> String.trim()
     str |> String.replace(sentence_join, " ") |> String.replace("  ", " ")
   end
 
   defp make_sentence(str) do
-    str = String.strip(str)
+    str = String.trim(str)
 
     if String.ends_with?(str, ".") do
       str <> @def_joins[:words]
@@ -80,5 +97,4 @@ defmodule Elixilorem.GetSum do
   end
 
   defp make_words(str), do: str |> String.replace(~r/\W/, " ") |> String.replace(~r/\s+/, " ")
-
 end
--- mix.exs
+++ mix.exs
@@ -2,27 +2,33 @@ defmodule Elixilorem.Mixfile do
   use Mix.Project
 
   def project do
-    [ app: :elixilorem,
+    [
+      app: :elixilorem,
       version: "0.0.1",
-      elixir: "~> 1.0.2",
+      elixir: ">= 1.7.3",
       description: "A Lorem Ipsum generator for Elixir",
-      package: package,
-      deps: [] ]
+      package: package(),
+      deps: []
+    ]
   end
 
-  def application, do:
-    [env: [
-      extension: ".txt",
-      flavors: ["lorem_ipsum", "iliad", "frankenstein", "lorem_ipsum", "riker", "cat"],
-      flavor: "lorem_ipsum",
-      format: "text",
-      joins: [paragraphs: "\n", sentences: ". ", words: " "]
-    ]]
+  def application,
+    do: [
+      env: [
+        extension: ".txt",
+        flavors: ["lorem_ipsum", "iliad", "frankenstein", "lorem_ipsum", "riker", "cat"],
+        flavor: "lorem_ipsum",
+        format: "text",
+        joins: [paragraphs: "\n", sentences: ". ", words: " "]
+      ]
+    ]
 
   defp package do
-    [ files: ~w(lib priv mix.exs README.md LICENSE.md),
-      contributors: ["Garrett Amini"],
+    [
+      files: ~w(lib priv mix.exs README.md LICENSE.md),
+      contributors: ["Garrett Amini", "Mike Martinson"],
       licenses: ["MIT"],
-      links: %{ "Github": "https://github.com/mgamini/elixilorem"} ]
+      links: %{Github: "https://github.com/mgamini/elixilorem"}
+    ]
   end
 end
